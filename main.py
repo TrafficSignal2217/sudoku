5ac0418ede7ede304abd5290bf32a9999462e48d
TrafficSignal2217 <loganbrown2217@gmail.com>
2024-11-23 12:53:13 -0500

    show screen

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import pygame
 
+
+
+
 def main():
-    print("Hi guys")
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        # poll for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("purple")
+
+        # RENDER YOUR GAME HERE
+
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limits FPS to 60
+
+    pygame.quit()
+
+
+
+
+
 
 if __name__ == "__main__":
     main()

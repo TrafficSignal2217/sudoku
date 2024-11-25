766f5b21bb6057ddedc1bd8c267b37dc7d8fc973
TrafficSignal2217 <loganbrown2217@gmail.com>
2024-11-24 23:57:48 -0500

    Cleaned up code; added comments; added TODOs

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import pygame
-from Sudoku_start_screen import *
-from grid import Grid
+from main_menu import *
+from board_render import Board_Render
 from sudoku_generator import *
 
 
 
 
 def main():
-    # pygame setup
+    ### pygame setup ###
     pygame.init()
     clock = pygame.time.Clock()
 
-    # Setup the display
-    screen_info = pygame.display.Info()
+    ### Setup the display ###
+    screen_info = pygame.display.Info()  # We will get the user's resolution and use it for the games resolution
     screen_width, screen_height = screen_info.current_w, screen_info.current_h
     screen = pygame.display.set_mode((screen_width, screen_height))
 
-    # Game Variables
+    ### Game Variables ###
     states = {
         "main_menu" : 0,
         "playing" : 1,
         "win" : 2,
         "lose" : 3
     }
-    game_state = states["main_menu"]
+    game_state = states["playing"]
 
-    grid = Grid(128, 96, 128, "black")
-    backdrop_color = "blanchedalmond"
+    ### Load images, fonts, and everything else that will be rendered ###
+    fonts = {
+        "header":pygame.font.Font(None, 80),  # None font is freesansbold, the default pygame font
+        "subheader":pygame.font.Font(None, 70),
+        "body":pygame.font.Font(None, 50),
+    }
+    main_menu_backdrop = pygame.image.load('project_image.png')
+    background_color = "blanchedalmond"
+    # This is the grid of lines and numbers that is drawn for the sudoku board
+    rendered_board = Board_Render(left_pos=128, top_pos=96, cell_size=128, line_color="black")
 
 
     running = True
     while running:
-        # poll for events
+        ### poll for events ###
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 # Press escape to exit the game
@@ -41,18 +49,19 @@ def main():
                 running = False
 
 
-        # Render the game
+        ### Render the game ###
         if game_state == states["main_menu"]:
-            start_screen()
-            screen.fill("black")
+            main_menu(screen, screen_width, screen_height, main_menu_backdrop, fonts)
         elif game_state == states["playing"]:
-            screen.fill(backdrop_color)
-            grid.draw(screen)
+            screen.fill(background_color)
+            rendered_board.draw(screen)
+        # TODO: Add win and lose game states
 
 
         # This draws what was rendered to the screen
         pygame.display.flip()
-        clock.tick(60)  # limits FPS to 60
+        # limit FPS to 60
+        clock.tick(60)
 
     pygame.quit()
 
---
+++ main_menu.py
@@ -0,0 +1,31 @@
+import pygame
+
+def main_menu(screen, screen_width, screen_height, backdrop, fonts):
+    backdrop = pygame.image.load('project_image.png')
+    screen.blit(backdrop, (0, 0))
+
+    header_text = "Welcome to Sudoku"
+    header_surf = fonts["header"].render(header_text, 1, "black")
+    header_rect = header_surf.get_rect(center=(screen_width // 2, 280))
+    screen.blit(header_surf, header_rect)
+
+    subheader_text = "Select Game Mode: "
+    subheader_surf = fonts["subheader"].render(subheader_text, 1, "black")
+    subheader_rect = subheader_surf.get_rect(center=(screen_width // 2, screen_height // 2))
+    screen.blit(subheader_surf, subheader_rect)
+
+    pygame.draw.rect(screen, "green", [700, 700, 150, 50])
+    pygame.draw.rect(screen, "yellow", [900, 700, 150, 50])
+    pygame.draw.rect(screen, "red", [1100, 700, 150, 50])
+    button1 = "Easy"
+    button2 = "Medium"
+    button3 = "Hard"
+    button1_surf = fonts["body"].render(button1, 1, "white")
+    button2_surf = fonts["body"].render(button2, 1, "white")
+    button3_surf = fonts["body"].render(button3, 1, "white")
+    button1_rect = button1_surf.get_rect(center=(775, 725))
+    button2_rect = button2_surf.get_rect(center=(975, 725))
+    button3_rect = button3_surf.get_rect(center=(1175, 725))
+    screen.blit(button1_surf, button1_rect)
+    screen.blit(button2_surf, button2_rect)
+    screen.blit(button3_surf, button3_rect)
